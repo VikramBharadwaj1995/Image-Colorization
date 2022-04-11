598e9147043ed2a6eff61d1b32c7b78d3bfbc492
Sudhanva Narayana <nsudhanva@gmail.com>
2022-04-11 15:23:08 -0400

    added l a b extraction

--- colorize_data.py
+++ colorize_data.py
@@ -4,11 +4,14 @@ from torch.utils.data import Dataset
 import torchvision.transforms as T
 import torch
 import os
+from skimage.color import rgb2lab
+import numpy as np
 
 from torchvision.transforms.functional import resize
 
-#Import PIl.Image to read image data
+# Import PIl.Image to read image data
 import cv2
+
 
 class ColorizeData(Dataset):
     def __init__(self, landscape_dataset, data_directory):
@@ -17,25 +20,27 @@ class ColorizeData(Dataset):
         self.landscape_dataset = landscape_dataset
         self.data_directory = data_directory
         self.input_transform = T.Compose([T.ToTensor(),
-                                          T.Resize(size=(224,224)),
+                                          #   T.Resize(size=(256,256)),
                                           T.Grayscale(),
                                           T.Normalize((0.5), (0.5))
                                           ])
         # Use this on target images(colorful ones)
         self.target_transform = T.Compose([T.ToTensor(),
-                                           T.Resize(size=(224,224)),
+                                           #   T.Resize(size=(256,224)),
                                            T.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
-    
+
     def __len__(self) -> int:
         # return Length of dataset
         return len(self.landscape_dataset)
-    
+
     def __getitem__(self, index: int) -> Tuple[torch.Tensor, torch.Tensor]:
         # Return the input tensor and output tensor for training
-        image_path = os.path.join(self.data_directory, self.landscape_dataset[index])
-        
-        color_image = cv2.imread(image_path)
-        grayscale_image = self.input_transform(color_image)
-        color_image = self.target_transform(color_image)
+        image_path = os.path.join(
+            self.data_directory, self.landscape_dataset[index])
 
-        return (grayscale_image, color_image)
+        color_image = cv2.imread(image_path)
+
+        l_channel, a, b = cv2.split(color_image)
+        ab = cv2.merge((a, b))
+
+        return (l_channel, ab)
